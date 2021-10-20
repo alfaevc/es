089bf63b45b02077eb8786f7e0a556b4706a31b3
Alvin Pan <qp2134@columbia.edu>
2021-10-19 22:14:53 -0400

    minor code fix

--- es.py
+++ es.py
@@ -21,7 +21,6 @@ def _get_action(s, theta):
   return a
 
 def rl_fn(theta, env):
-  assert len(theta) == 5
   # START HIDE
   done = False
   s = env.reset()
@@ -34,49 +33,48 @@ def rl_fn(theta, env):
   return total_reward
 
 def vanilla_gradient(theta, F, sigma=1, N=100):
-  assert len(theta) == 5
-  epsilons = np.random.multivariate_normal(np.zeros(5), np.identity(5), size=N)
+  epsilons = np.random.standard_normal(size=(N, theta.size))
   fn = lambda x: F(theta + sigma * x) * x
   return np.mean(np.array(list(map(fn, epsilons))), axis=0)/sigma
 
 def FD_gradient(theta, F, sigma=1, N=100):
-  assert len(theta) == 5
-  epsilons = np.random.multivariate_normal(np.zeros(5), np.identity(5), size=N)
+  epsilons = np.random.standard_normal(size=(N, theta.size))
   fn = lambda x: (F(theta + sigma * x) - F(theta)) * x
   return np.mean(np.array(list(map(fn, epsilons))), axis=0)/sigma
 
 def AT_gradient(theta, F, sigma=1, N=100):
-  assert len(theta) == 5
-  epsilons = np.random.multivariate_normal(np.zeros(5), np.identity(5), size=N)
+  epsilons = np.random.standard_normal(size=(N, theta.size))
   fn = lambda x: (F(theta + sigma * x) - F(theta - sigma * x)) * x
   return np.mean(np.array(list(map(fn, epsilons))), axis=0)/sigma/2
     
 def gradascent(theta0, F, method=None, eta=1e-2, max_epoch=200, N=100):
   theta = np.copy(theta0)
+  theta_len = theta.size
   accum_rewards = np.zeros(max_epoch)
   for i in range(max_epoch):
     accum_rewards[i] = F(theta)
     print("The return for episode {0} is {1}".format(i, accum_rewards[i]))
     if method == "FD":
-      theta += eta * FD_gradient(theta, F, N=N)
+      theta += eta * FD_gradient(theta, F, theta_len, N=N)
     elif method == "AT":
-      theta += eta * AT_gradient(theta, F, N=N)
+      theta += eta * AT_gradient(theta, F, theta_len, N=N)
     else: #vanilla
-      theta += eta * vanilla_gradient(theta, F, N=N)
+      theta += eta * vanilla_gradient(theta, F, theta_len, N=N)
   return theta, accum_rewards
 
 """The cell below applies your CMA-ES implementation to the RL objective you've defined in the cell above."""
 if __name__ == '__main__':
   env = gym.make('CartPole-v0')
+  theta_dim = 5
   fn_with_env = functools.partial(rl_fn, env=env)
   num_seeds = 5
   max_epoch = 150
   N = 20
   res = np.zeros((num_seeds, max_epoch))
-  method = "AT"
+  method = "FD"
 
   for k in tqdm.tqdm(range(num_seeds)):
-    theta0 = np.random.multivariate_normal(np.zeros(5), np.identity(5))
+    theta0 = np.random.standard_normal(size=theta_dim)
 
     # epsilons = np.random.multivariate_normal(np.zeros(5), np.identity(5), size=2)
     # print(epsilons)
