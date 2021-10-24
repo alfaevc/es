683ab5e78471f3b269d88d9070deb9b8f99015f4
yzhao3685 <83072588+yzhao3685@users.noreply.github.com>
2021-10-23 22:55:05 -0400

    Update ES 2021102301.py

--- ES 2021102301.py
+++ ES 2021102301.py
@@ -50,7 +50,8 @@ def FD_gradient(theta, F, sigma=1, N=100):
   return np.mean(np.array(list(map(fn, epsilons))), axis=0)/sigma
 
 def AT_gradient(theta, F, sigma=1, N=100):
-  epsilons = np.random.standard_normal(size=(N, theta.size))
+  #epsilons = np.random.standard_normal(size=(N, theta.size))
+  epsilons=orthogonal_epsilons(theta)
   fn = lambda x: (F(theta + sigma * x) - F(theta - sigma * x)) * x
   return np.mean(np.array(list(map(fn, epsilons))), axis=0)/sigma/2
 
